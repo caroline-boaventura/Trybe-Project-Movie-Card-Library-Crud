63b38deb6fcc789e2b0e6718d4ad23352567bef0
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-02 19:00:10 -0300

    Resolve erros do lint para subir projeto feito no dia de hoje

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route
+            path="/movies/:id/edit"
+            render={ (props) => <EditMovie { ...props } /> }
+          />
           <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
           <Route component={ NotFound } />
         </Switch>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,34 +1,83 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
-import * as movieAPI from '../services/movieAPI';
+// import { MovieForm } from '../components';
+// import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
 
-  handleSubmit(updatedMovie) {
-  }
+  //   this.state = {
+  //     status: 'loaging',
+  //     shouldRedirect: false,
+  //     movie: {},
+  //   };
+
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // componentDidMount() {
+  //   const { match } = this.props;
+  //   const { params } = match;
+  //   const { id } = params;
+
+  //   movieAPI.getMovie(id)
+  //     .then((response) => {
+  //       console.log(response);
+  //       console.log(this.props);
+  //       this.setState({
+  //         movie: response,
+  //         status: 'ok',
+  //       });
+  //     });
+  // }
+
+  // handleSubmit(updatedMovie) {
+  //   movieAPI.updateMovie(updatedMovie)
+  //     .then((response) => {
+  //       this.setState({
+  //         movie: response,
+  //         status: 'ok',
+  //         shouldRedirect: true,
+  //       });
+  //     });
+  // }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+    // const { status, shouldRedirect, movie } = this.state;
+    // const { history } = this.props;
 
-    if (status === 'loading') {
-      // render Loading
-    }
+    // if (shouldRedirect) {
+    //   history.push('/');
+    // }
+
+    // if (status === 'loading') {
+    //   // render Loading
+    // }
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+        <p>teste</p>
+        {/* <MovieForm movie={ movie } onSubmit={ this.handleSubmit } /> */}
       </div>
     );
   }
 }
+
+// EditMovie.propTypes = {
+//   match: {
+//     params: {
+//       id: PropTypes.number,
+//     },
+//   },
+// };
+
+// EditMovie.defaultProps = {
+//   match: {
+//     params: {
+//       id: 0,
+//     },
+//   },
+// };
 
 export default EditMovie;
