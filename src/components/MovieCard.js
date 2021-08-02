671c54c335eac5e2bf95d45fe1bf7b861600bf98
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-02 16:59:20 -0300

    Resolve requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,7 +10,7 @@ class MovieCard extends React.Component {
       <div data-testid="movie-card">
         <h2>{ movie.title }</h2>
         <p>{ movie.storyline }</p>
-        <Link to={ `movies/:${movie.id}` }>VER DETALHES</Link>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
