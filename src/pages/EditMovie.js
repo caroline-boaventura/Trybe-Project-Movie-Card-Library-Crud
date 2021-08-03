035227fca9769052d737262ec68a1a6f1d1c73a7
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-03 16:40:39 -0300

    Resolve requisitos 5 e 6

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,83 +1,86 @@
 import React, { Component } from 'react';
-
-// import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.state = {
-  //     status: 'loaging',
-  //     shouldRedirect: false,
-  //     movie: {},
-  //   };
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
 
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  // componentDidMount() {
-  //   const { match } = this.props;
-  //   const { params } = match;
-  //   const { id } = params;
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
 
-  //   movieAPI.getMovie(id)
-  //     .then((response) => {
-  //       console.log(response);
-  //       console.log(this.props);
-  //       this.setState({
-  //         movie: response,
-  //         status: 'ok',
-  //       });
-  //     });
-  // }
+    movieAPI.getMovie(parseInt(id, 10))
+      .then((response) => {
+        this.setState({
+          movie: response,
+          status: 'ok',
+        });
+      });
+  }
 
-  // handleSubmit(updatedMovie) {
-  //   movieAPI.updateMovie(updatedMovie)
-  //     .then((response) => {
-  //       this.setState({
-  //         movie: response,
-  //         status: 'ok',
-  //         shouldRedirect: true,
-  //       });
-  //     });
-  // }
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie)
+      .then((response) => {
+        this.setState({
+          movie: response,
+          status: 'ok',
+          shouldRedirect: true,
+        });
+      });
+  }
 
   render() {
-    // const { status, shouldRedirect, movie } = this.state;
-    // const { history } = this.props;
+    const { status, shouldRedirect, movie } = this.state;
+    const { history } = this.props;
 
-    // if (shouldRedirect) {
-    //   history.push('/');
-    // }
+    if (shouldRedirect) {
+      history.push('/');
+    }
 
-    // if (status === 'loading') {
-    //   // render Loading
-    // }
+    if (status === 'loading') {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="edit-movie">
-        <p>teste</p>
-        {/* <MovieForm movie={ movie } onSubmit={ this.handleSubmit } /> */}
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
 }
 
-// EditMovie.propTypes = {
-//   match: {
-//     params: {
-//       id: PropTypes.number,
-//     },
-//   },
-// };
+EditMovie.propTypes = {
+  match: {
+    params: {
+      id: PropTypes.string,
+    },
+  },
+  history: {
+    push: PropTypes.func,
+  },
+};
 
-// EditMovie.defaultProps = {
-//   match: {
-//     params: {
-//       id: 0,
-//     },
-//   },
-// };
+EditMovie.defaultProps = {
+  match: {
+    params: {
+      id: '0',
+    },
+  },
+  history: {
+    push: undefined,
+  },
+};
 
 export default EditMovie;
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-
-// import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
+  constructor(props) {
+    super(props);
 
-  // handleSubmit(newMovie) {
-  // }
+    this.state = {
+      movie: {},
+      shouldRedirect: false,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie)
+      .then((response) => {
+        this.setState({
+          movie: response,
+          shouldRedirect: true,
+        });
+      });
+  }
 
   render() {
+    const { movie, shouldRedirect } = this.state;
+    const { history } = this.props;
+
+    if (shouldRedirect) {
+      history.push('/');
+    }
+
     return (
       <div data-testid="new-movie">
-        <p>teste</p>
-        {/* <MovieForm onSubmit={ this.handleSubmit } /> */}
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
 }
+
+NewMovie.propTypes = {
+  history: {
+    push: PropTypes.func,
+  },
+};
+
+NewMovie.defaultProps = {
+  history: {
+    push: undefined,
+  },
+};
 export default NewMovie;
